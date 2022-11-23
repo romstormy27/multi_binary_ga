5c885bd113639d176cb4e56167571be90aac823d
romstormy27 <fahruryanfuhrer27@gmail.com>
2022-11-23 18:45:08 +0700

    change streamlit fire

--- src/front_end.py
+++ src/front_end.py
@@ -90,7 +90,7 @@ with st.form(key="input_params"):
         }
 
         with st.spinner("Running..."):
-            res = requests.post("http://api:80/run/", json=input_params).json()
+            res = requests.post("https://multiga.herokuapp.com/run/", json=input_params).json()
 
         if res["msg"] != None:
             st.error(str(res["msg"]))
